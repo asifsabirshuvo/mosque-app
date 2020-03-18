a55bff431f18f8c207ec3b08a4ec84f50d1eb539
Asif Sabir Shuvo <asifsabirshuvo@gmail.com>
2020-03-18 18:18:33 +0600

    just removed all alarms

--- app/src/main/java/com/recipeworld/mosqueapp/MainActivity.java
+++ app/src/main/java/com/recipeworld/mosqueapp/MainActivity.java
@@ -45,7 +45,7 @@ import java.util.Locale;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     ArrayList<String> col0 = new ArrayList<>();
     ArrayList<String> col1 = new ArrayList<>();
@@ -61,9 +61,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     TextView tvJumaAzan, tvJumaIqamah;
     TextView tvNote, tvSunrise;
 
-    // Spinner element
-    Spinner fajrSpinner, duhrSpinner, asrSpinner,
-            maghribSpinner, ishaSpinner, jumaSpinner;
 
 
     @Override
@@ -88,43 +85,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         tvNote = (TextView) findViewById(R.id.note);
         tvSunrise = (TextView) findViewById(R.id.sunrise);
 
-
-        fajrSpinner = (Spinner) findViewById(R.id.fajr_spinner);
-        duhrSpinner = (Spinner) findViewById(R.id.duhr_spinner);
-        asrSpinner = (Spinner) findViewById(R.id.asr_spinner);
-        maghribSpinner = (Spinner) findViewById(R.id.maghrib_spinner);
-        ishaSpinner = (Spinner) findViewById(R.id.isha_spinner);
-        jumaSpinner = (Spinner) findViewById(R.id.juma_spinner);
-
-        // Spinner click listener
-        fajrSpinner.setOnItemSelectedListener(this);
-        duhrSpinner.setOnItemSelectedListener(this);
-        asrSpinner.setOnItemSelectedListener(this);
-        maghribSpinner.setOnItemSelectedListener(this);
-        ishaSpinner.setOnItemSelectedListener(this);
-        jumaSpinner.setOnItemSelectedListener(this);
-
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("NONE");
-        categories.add("15 min");
-        categories.add("30 min");
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        fajrSpinner.setAdapter(dataAdapter);
-        duhrSpinner.setAdapter(dataAdapter);
-        asrSpinner.setAdapter(dataAdapter);
-        maghribSpinner.setAdapter(dataAdapter);
-        ishaSpinner.setAdapter(dataAdapter);
-        jumaSpinner.setAdapter(dataAdapter);
-
-
+      // Spinner Drop down elements
         //calling the website by default
 
         getWebsite();
@@ -174,25 +135,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-
-//                        Log.d("xx", String.valueOf(col0.size()));
-//                        Log.d("xx", String.valueOf(col1.size()));
-//                        Log.d("xx", String.valueOf(col2.size()));
-//
-//
-//                        for (String xxs : col0) {
-//                            Log.d("xx", xxs);
-//                            Log.d("xx", "\n");
-//                        }
-//                        for (String xxs : col1) {
-//                            Log.d("xx", xxs);
-//                            Log.d("xx", "\n");
-//                        }
-//                        for (String xxs : col2) {
-//                            Log.d("xx", xxs);
-//                            Log.d("xx", "\n");
-//                        }
 
                         setToStorage(col0, col1, col2);
 
@@ -255,13 +197,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         String note = preferences.getString("note", "");
 
 
-        int fajrAlert = preferences.getInt("fajrAlert", 0);
-        int duhrAlert = preferences.getInt("duhrAlert", 0);
-        int asrAlert = preferences.getInt("asrAlert", 0);
-        int maghribAlert = preferences.getInt("maghribAlert", 0);
-        int ishaAlert = preferences.getInt("ishaAlert", 0);
-        int jumaAlert = preferences.getInt("jumaAlert", 0);
-
 
         if (TextUtils.isEmpty(fajrAzan)) {
             //so things are not previously called; call the server
@@ -289,32 +224,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             tvJumaIqamah.setText(jumaIqamah);
 
             tvNote.setText(note);
-
-            //setting the selector view
-            fajrSpinner.setSelection(fajrAlert, true);
-            duhrSpinner.setSelection(duhrAlert, true);
-            asrSpinner.setSelection(asrAlert, true);
-            maghribSpinner.setSelection(maghribAlert, true);
-            ishaSpinner.setSelection(ishaAlert, true);
-            jumaSpinner.setSelection(jumaAlert, true);
-
-            //call the all alert setter
-            if (fajrAlert != 0)
-                ScheduleDailyN(tvFajrIqamah.getText().toString(), 0, fajrAlert);
-            if (duhrAlert != 0)
-                ScheduleDailyN(tvDuhrIqamah.getText().toString(), 1, duhrAlert);
-            if (asrAlert != 0)
-                ScheduleDailyN(tvAsrIqamah.getText().toString(), 2, asrAlert);
-            if (maghribAlert != 0)
-                ScheduleDailyN(tvMaghribIqamah.getText().toString(), 3, maghribAlert);
-            if (ishaAlert != 0)
-                ScheduleDailyN(tvIshaIqamah.getText().toString(), 4, ishaAlert);
-            if (jumaAlert != 0)
-                ScheduleDailyN(tvJumaIqamah.getText().toString(), 5, jumaAlert);
-
-
-            //now check for intents check purpose
-            checkForExistingAlarms();
 
 
         }
@@ -345,204 +254,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        int ids = parent.getId();
-        if (ids == R.id.fajr_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("fajrAlert", position);
-            editor.apply();
-
-            //resetting alarm mananger
-            if (tvFajrIqamah.getText().toString().length() > 1) {
-                if (position == 0)
-                    RemoveDailyN(0);
-                else
-                    ScheduleDailyN(tvFajrIqamah.getText().toString()
-                            , 0, position);
-
-            }
-
-        } else if (ids == R.id.duhr_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("duhrAlert", position);
-            editor.apply();
-
-            //resetting alarm mananger
-            if (tvDuhrIqamah.getText().toString().length() > 1) {
-                if (position == 0)
-                    RemoveDailyN(1);
-                else
-                    ScheduleDailyN(tvDuhrIqamah.getText().toString()
-                            , 1, position);
-            }
-
-        } else if (ids == R.id.asr_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("asrAlert", position);
-            editor.apply();
-
-            //resetting alarm mananger
-            if (tvAsrIqamah.getText().toString().length() > 1) {
-                if (position == 0)
-                    RemoveDailyN(2);
-                else
-                    ScheduleDailyN(tvAsrIqamah.getText().toString()
-                            , 2, position);
-            }
-
-        } else if (ids == R.id.maghrib_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("maghribAlert", position);
-            editor.apply();
-            //resetting alarm mananger
-            if (tvMaghribIqamah.getText().toString().length() > 1) {
-                if (position == 0)
-                    RemoveDailyN(3);
-                else
-                    ScheduleDailyN(tvMaghribIqamah.getText().toString()
-                            , 3, position);
-            }
-
-        } else if (ids == R.id.isha_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("ishaAlert", position);
-            editor.apply();
-
-            //resetting alarm mananger
-            if (tvIshaIqamah.getText().toString().length() > 1) {
-                if (position == 0)
-                    RemoveDailyN(4);
-                else
-
-                    ScheduleDailyN(tvIshaIqamah.getText().toString()
-                            , 4, position);
-            }
-        } else if (ids == R.id.juma_spinner) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("jumaAlert", position);
-            editor.apply();
-
-
-            //resetting alarm mananger
-            if (tvJumaIqamah.getText().toString().length() > 1) {
-
-                if (position == 0)
-                    RemoveDailyN(5);
-                else
-                    ScheduleDailyN(tvJumaIqamah.getText().toString()
-                            , 5, position);
-            }
-        } else {
-
-        }
-
-    }
-
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-
-
-    public void ScheduleDailyN(String time, int prayerNo, int beforeIndex) {
-
-        String time1[] = time.trim().split(" ");
-        String AMPM = time1[1].trim();
-        String[] hrmm = time1[0].trim().split(":");
-        int hr = Integer.parseInt(hrmm[0].trim());
-        int mm = Integer.parseInt(hrmm[1].trim());
-
-        Log.d("time", AMPM + "\n" + hr + "\n" + mm);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, hr);
-        calendar.set(Calendar.MINUTE, mm);
-
-        if (AMPM.equals("AM"))
-            calendar.set(Calendar.AM_PM, Calendar.AM);
-        else
-            calendar.set(Calendar.AM_PM, Calendar.PM);
-
-        // Check if the Calendar time is in the past
-        if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
-            Log.e("past","time is in past");
-            calendar.add(Calendar.DAY_OF_YEAR, 1); // it will tell to run to next day
-        }
-
-
-        Intent intent = new Intent(this, BroadCastClass.class);
-
-        if(prayerNo==0)intent.putExtra("pName","FAJR");
-        else if(prayerNo==1)intent.putExtra("pName","DUHR");
-        else if(prayerNo==2)intent.putExtra("pName","ASR");
-        else if(prayerNo==3)intent.putExtra("pName","MAGHRIB");
-        else if(prayerNo==4)intent.putExtra("pName","ISHA");
-        else if(prayerNo==5)intent.putExtra("pName","JUMA");
-        else intent.putExtra("pName","PRAYER");
-
-
-
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                this.getApplicationContext(), prayerNo, intent, 0);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()
-                , AlarmManager.INTERVAL_DAY, pendingIntent);
-
-    }
-
-    public void RemoveDailyN(int prayer) {
-        Log.d("alarm",prayer+" is turned OFF");
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent myIntent = new Intent(getApplicationContext(), BroadCastClass.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                getApplicationContext(), prayer, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        alarmManager.cancel(pendingIntent);
-    }
-
-    public void checkForExistingAlarms() {
-        boolean alarmUp0 = (PendingIntent.getBroadcast(getApplicationContext(), 0,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-
-        boolean alarmUp1 = (PendingIntent.getBroadcast(getApplicationContext(), 1,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-        boolean alarmUp2 = (PendingIntent.getBroadcast(getApplicationContext(), 2,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-        boolean alarmUp3 = (PendingIntent.getBroadcast(getApplicationContext(), 3,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-        boolean alarmUp4 = (PendingIntent.getBroadcast(getApplicationContext(), 4,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-        boolean alarmUp5 = (PendingIntent.getBroadcast(getApplicationContext(), 5,
-                new Intent(this, BroadCastClass.class),
-                PendingIntent.FLAG_NO_CREATE) != null);
-
-
-        if (alarmUp0)
-            Log.d("alarm", "fajr is active");
-        if (alarmUp1)
-            Log.d("alarm", "duhr is active");
-        if (alarmUp2)
-            Log.d("alarm", "asr is active");
-        if (alarmUp3)
-            Log.d("alarm", "magrib is active");
-        if (alarmUp4)
-            Log.d("alarm", "isha is active");
-        if (alarmUp5)
-            Log.d("alarm", "juma is active");
-    }
 
 }
